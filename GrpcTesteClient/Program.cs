cd2a7f48cdbfbeaadd39f8e569f796d82e0a4316
IgorSapia <igor.sapia@gmail.com>
2024-02-14 00:33:51 -0300

    Adicionar arquivos de projeto.

---
+++ GrpcTesteClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using GrpcTeste;
+using GrpcTeste.Services;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5080/");
+
+var client = new Greeter.GreeterClient(channel);
+var req = new HelloRequest
+{
+    Name = "Igor"
+};
+
+var reply = await client.SayHelloAsync(req);
+
+Console.WriteLine("Saudacao: " + reply.Message);
+Console.WriteLine("Pressione qualquer coisa para sair...");
+Console.ReadKey();
